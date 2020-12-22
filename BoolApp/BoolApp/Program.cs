a553d0a383c2d1f94bc346553d84356c933b37e7
Maria Hou <maria.hou@yahoo.com>
2020-12-21 23:15:05 -0500

    add boolean app

---
+++ BoolApp/BoolApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BoolApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess what the cat is doing!");
+            string activity = Console.ReadLine();
+            bool guess = activity == "sleeping";
+
+            do
+            {
+                switch (activity)
+                {
+                    case "eating":
+                        Console.WriteLine("\nWhile cats do love to eat, this one is doing something different.");
+                        Console.WriteLine("Guess what the cat is doing!");
+                        activity = Console.ReadLine();
+                        break;
+
+                    case "hunting":
+                        Console.WriteLine("\nHmm, not quite, this cat is quite lazy.");
+                        Console.WriteLine("Guess what the cat is doing!");
+                        activity = Console.ReadLine();
+                        break;
+
+                    case "sleeping":
+                        Console.WriteLine("\nAhh yes, this cat loves to sleep.");
+                        guess = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("\nHmm, try 'sleeping', 'eating', or 'hunting'.");
+                        activity = Console.ReadLine();
+                        break;
+                }
+
+            }
+            while (!guess);
+            Console.ReadLine();
+
+        }
+    }
+}
