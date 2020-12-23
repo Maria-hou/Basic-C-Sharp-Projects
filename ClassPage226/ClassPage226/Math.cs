a747896d8bbceb09a22d773cc9f37c115d6a633b
Maria Hou <maria.hou@yahoo.com>
2020-12-23 17:36:28 -0500

    add methods overload

---
+++ ClassPage226/ClassPage226/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPage226
+{
+    public class Math
+    {
+        public static int DoMath(int input)
+        {
+            return input + 231;
+        }
+
+        public static int DoMath(decimal input2)
+        {
+            return Convert.ToInt32(input2 - 24.5m);
+        }
+
+        public static int DoMath(string input3)
+        {
+            int num = Convert.ToInt32(input3);
+            return (num * 5);
+        }
+    }
+}
---
+++ ClassPage226/ClassPage226/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPage226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Enter a number to add to 231: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input + " plus 231 is " + Math.DoMath(input));
+
+            Console.WriteLine("Enter a decimal to subtract 24.5 from: ");
+            decimal input2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(input2 + " minus 24.5 is " + Math.DoMath(input2));
+
+            Console.WriteLine("Enter a number to multiply by 5: ");
+            string input3 = Console.ReadLine();
+            Console.WriteLine(input3 + " multiplied by 5 is " + Math.DoMath(input3));
+
+
+            Console.ReadLine();
+        }
+    }
+}
