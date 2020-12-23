6fb90af8991f8a0e468f3668d579be43caf906f8
Maria Hou <maria.hou@yahoo.com>
2020-12-22 20:13:52 -0500

    Add Math class console assignment

---
+++ ClassAndMethods1/ClassAndMethods1/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods1
+{
+    public class Math
+    {
+        public static int Addition (int input)
+        {
+            return input + 13;
+        }
+
+        public static int Multiply(int input)
+        {
+            return input * 3;
+        }
+
+        public static int Subtraction(int input)
+        {
+            return input - 239;
+        }
+    }
+}
---
+++ ClassAndMethods1/ClassAndMethods1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Enter a numer so we can do some basic math: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(input+ " plus 13 equals " + Math.Addition(input));
+            Console.ReadLine();
+
+            Console.WriteLine(input + " multiplied 3 equals " + Math.Multiply(input));
+            Console.ReadLine();
+
+            Console.WriteLine(input + " minus 239 equals " + Math.Subtraction(input));
+            Console.ReadLine();
+        }
+    }
+}
