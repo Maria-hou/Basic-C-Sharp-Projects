7515fad151fbae426597b0b3e85d1e80fc9b45bb
Maria Hou <maria.hou@yahoo.com>
2020-12-23 20:10:29 -0500

    add page 227 assignment

---
+++ ClassPage227/ClassPage227/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPage227
+{
+    public class Math
+    {
+        public static int DoMath(int a, int b =3 )
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ClassPage227/ClassPage227/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPage227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number to multiply the first by, this is optional. If no number is entered, a default of 3 will be used: ");
+            string b = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(b))
+            {
+                Console.WriteLine(a + " multiplied by 3 is " + Math.DoMath(a));
+            }
+            else
+            {
+                Console.WriteLine(a + " multiplied by " + b + " is " + Math.DoMath(a, Convert.ToInt32(b)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
