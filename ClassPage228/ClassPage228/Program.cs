ab21e0374e509d370c6e52da23f70525c80f7d0f
Maria Hou <maria.hou@yahoo.com>
2020-12-23 20:56:22 -0500

    commit page 228 assignment

---
+++ ClassPage228/ClassPage228/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPage228
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Multiply(2, 8);
+            math.Multiply(a: 2, b: 8);
+
+            Console.ReadLine();
+        }
+    }
+}
