81442a0555de8f2895ba22c7339879290e5d791d
Maria Hou <maria.hou@yahoo.com>
2020-12-28 18:57:17 -0500

    add enum assignment

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day of the week is it?");
+            string input = (Console.ReadLine()).ToLower();
+            
+            try
+            {
+                Days day = (Days)Enum.Parse(typeof(Days), input);
+                Console.WriteLine("Have a good " + day);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+    public enum Days
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    }
+}
