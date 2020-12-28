851bb79db3cc6f17643593e09c95006f14f2ac77
Maria Hou <maria.hou@yahoo.com>
2020-12-28 15:23:27 -0500

    add overload operators assignment

---
+++ OverloadOperators/OverloadOperators/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return (employee1.ID == employee2.ID);
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return (employee1.ID != employee2.ID);
+        }
+
+    }
+}
---
+++ OverloadOperators/OverloadOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Anna = new Employee() { Name = "Anna", ID = 001 };
+            Employee Lauren = new Employee() { Name = "Lauren", ID = 002 };
+
+            Console.WriteLine("Both employees have the same IDs." + (Anna == Lauren));
+            Console.WriteLine("Both employees do not have the same IDs." + (Anna != Lauren));
+        }
+    }
+}
