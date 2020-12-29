c4cdeb00d8b1d65e472d9ee0b5bfc8295b29e012
Maria Hou <maria.hou@yahoo.com>
2020-12-29 14:31:14 -0500

    add log text assignment

---
+++ Logs/Logs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string input = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter("C:\\Users\\maria\\Desktop\\Basic_C#_Programs\\log2.txt", true))
+            {
+                file.WriteLine(input);
+            }
+
+            string text = File.ReadAllText(@"C:\\Users\\maria\\Desktop\\Basic_C#_Programs\\log2.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+        
+
+    }
+}
