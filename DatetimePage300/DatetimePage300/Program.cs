64f9e6e16628141c0598d8d16ceb51687fdb01db
Maria Hou <maria.hou@yahoo.com>
2020-12-29 14:55:05 -0500

    add dateTime assignment

---
+++ DatetimePage300/DatetimePage300/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatetimePage300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("The time is now " + now);
+            Console.WriteLine("\nNow please eneter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("It will be {0} in {1} hours.", now.AddHours(input), input);
+            Console.ReadLine();
+        }
+    }
+}
