52cf40544641d922f35f0093445b3be49db44e35
Maria Hou <maria.hou@yahoo.com>
2020-12-29 16:47:04 -0500

    Add constant var assignment

---
+++ ConstVar/ConstVar/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    public class Message
+    {
+        public Message(string name) : this(name, 5000)
+        {
+        }
+
+        public Message(string name, int owed)
+        {
+            Name = name;
+            Amount = owed;
+            Console.WriteLine("\nWe are sorry to inform you {0}, you owe us a total of ${1} before you are allowed to access your funds.", name, Amount);
+        }
+
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ ConstVar/ConstVar/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            const string newYear = "Happy New Year!";
+            var member = new Dictionary<string, string>()
+            {
+                {"0-1000", "Basic" },
+                {"1000-5000", "Silver" },
+                {"5000-10000", "Gold" },
+                {"10000+", "Platinum" },
+            };
+            var tier = "platinum";
+            Console.WriteLine(newYear + "\n\nWelcome to Chase Bank {0}, you are a {1} member.", name, tier);
+            
+            
+
+            Message message = new Message(name);
+            Console.ReadLine();
+
+        }
+    }
+}
