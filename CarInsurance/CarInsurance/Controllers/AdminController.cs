7d2ef717e90ecb1de5a42c392ef6beafbfe7403f
Maria Hou <maria.hou@yahoo.com>
2021-01-05 00:19:06 -0500

    add CarInsurance app

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees.ToList();
+                var insureeVms = new List<Insuree>();
+
+                foreach (Insuree insuree in insurees)
+                {
+                    var insureeVm = new Insuree();
+                    insureeVm.Id = insuree.Id;
+                    insureeVm.FirstName = insuree.FirstName;
+                    insureeVm.LastName = insuree.LastName;
+                    insureeVm.EmailAddress = insuree.EmailAddress;
+                    insureeVm.Quote = insuree.Quote;
+                    insureeVms.Add(insureeVm);
+                }
+                return View(insureeVms);
+            }
+        }
+    }
+}
